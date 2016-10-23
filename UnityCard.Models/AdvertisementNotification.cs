ec0cf0e03b04c7decf41f500f6440c9be3716269
Lorenz\Lorenz Vercoutere <lorenzvercoutere@hotmail.com>
2016-10-23 15:18:32 +0200

    Models Backend

---
+++ UnityCard.Models/AdvertisementNotification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityCard.Models
+{
+    public class AdvertisementNotification
+    {
+        public int Id { get; set; }
+        public int RetailerId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ UnityCard.Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityCard.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Token { get; set; }
+        public string Language { get; set; }
+        public bool DisableNotifications { get; set; }
+    }
+}
