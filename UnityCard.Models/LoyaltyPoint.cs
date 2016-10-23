50ebec38b0b00498488e6a0283ee407120f10902
Lorenz Vercoutere <lorenzvercoutere@MBP-van-Lorenz.lan>
2016-10-23 14:19:59 +0200

    Models Backend toegevoegd

---
+++ UnityCard.Models/LoyaltyPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityCard.Models
+{
+    public class LoyaltyPoint
+    {
+        public int Id { get; set; }
+        public int Value { get; set; }
+        public DateTime Timestamp { get; set; } //Voor stephen: is datetime correct?
+        public int CustomerJunctionId { get; set; }
+
+    }
+}
---
+++ UnityCard.Models/Retailer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityCard.Models
+{
+    public class Retailer
+    {
+        public int Id { get; set; }
+        public int RetailerCategoryId { get; set; }
+        public string Name { get; set; }
+        public string Tagline { get; set; }
+        public bool Chain { get; set; }
+        public string LogoUrl { get; set; }
+
+    }
+}
